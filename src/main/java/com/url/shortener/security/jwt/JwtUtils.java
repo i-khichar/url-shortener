0b080c8e5106a0f850d1ec0925bd4ac0b4441276
Ishita Khichar <ishitakhichar@gmail.com>
2025-02-27 19:09:56 +0530

    Register user functionality added

--- src/main/java/com/url/shortener/security/jwt/JwtUtils.java
+++ src/main/java/com/url/shortener/security/jwt/JwtUtils.java
@@ -7,18 +7,20 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 import java.util.stream.Collectors;
 
+@Component
 public class JwtUtils {
 
     @Value("${jwt.secret}")
     private String jwtSecret;
 
-    @Value("$jwt.expiration}")
+    @Value("${jwt.expiration}")
     private int jwtExpirationMs;
     public String getJwtFromHeader(HttpServletRequest request){
         String bearerToken = request.getHeader("Authorization");
